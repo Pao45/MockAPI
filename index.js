47d5ffac9aab41afed99446b89f74564639d7957
Paola Nieves <paola.nieves25@gmail.com>
2018-12-15 14:35:35 -0400

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 
-const port = 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
